d29e3436aa11dc156afdf247124d787d83d675ef
Kreiser1 <kreiser2015@mail.ru>
2026-05-24 03:05:46 +0300

    ...

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Quizio
-Веб-приложение для многопользовательских соревнований.
+
+_Веб-приложение для многопользовательских соревнований._
+
+Команда для клонирования:
+
+```
+git clone https://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@github.com/Kreiser1/Quizio.git
+```
+
+Команда для установки:
+
+```
+py -m venv venv
+venv\Scripts\activate
+pip install -r requirements.txt
+```
+
+Команда для обновления:
+
+```
+git pull
+```
+
+Команда для запуска:
+
+```
+fastapi dev
+```
--- security.py
+++ security.py
@@ -3,7 +3,7 @@ from pwdlib.hashers.argon2 import Argon2Hasher
 from secrets import token_hex
 from jwt import PyJWT
 from time import time
-from config import AUTH_SECRET, TOKEN_SECRET, TOKEN_EXPIRATION, AUTH_COST, AUTH_COOLDOWN
+from config import AUTH_SECRET, TOKEN_SECRET, TOKEN_EXPIRATION, AUTH_COST
 from schema import RefreshToken, Password, AccessToken
 
 ACCESS_COOKIE='access_token'
@@ -12,8 +12,14 @@ REFRESH_COOKIE='refresh_token'
 jwt = PyJWT()
 argon2 = PasswordHash([Argon2Hasher(time_cost=4, memory_cost=AUTH_COST)])
 
-def compare(secret: str, hash: str):
-    return argon2.verify(secret, hash)
+def compare(secret: str, hash: str) -> bool:
+    return argon2.verify(secret + AUTH_SECRET, hash)
 
-def hash(secret: str):
-    return argon2.hash(secret)
+def hash(secret: str) -> str:
+    return argon2.hash(secret + AUTH_SECRET)
+
+def encode(token: dict) -> str:
+    return jwt.encode(token, TOKEN_SECRET, 'HS256')
+
+def decode(token: str) -> dict:
+    return jwt.decode(token, TOKEN_SECRET, 'HS256')
